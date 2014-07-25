2563ea13b418a9109373675c88301d5a81b4bd27
Rik Barwick <rik@creativitea.co.uk>
2014-07-25 13:22:48 +0100

    adjusted panel templates

--- public/sites/all/themes/omega/omega/panels/layouts/omega_12/omega_12_twocol_8_4/omega-12-twocol-8-4.tpl.php
+++ public/sites/all/themes/omega/omega/panels/layouts/omega_12/omega_12_twocol_8_4/omega-12-twocol-8-4.tpl.php
@@ -1,4 +1,4 @@
-<div class="panel-display omega-grid omega-12-twocol-3-9" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+<div class="panel-display omega-grid omega-12-twocol-8-4" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
   <div class="panel-panel grid-8">
     <div class="inside"><?php print $content['left']; ?></div>
   </div>
