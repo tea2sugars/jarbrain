328cb6566298a7605777ff2424073c2b70f62b26
Rik Barwick <rik@creativitea.co.uk>
2014-10-31 13:02:23 +0000

    Updated json2 and jcrop libraries

--- public/sites/all/libraries/jcrop/demos/crop.php
+++ public/sites/all/libraries/jcrop/demos/crop.php
@@ -3,7 +3,7 @@
 /**
  * Jcrop image cropping plugin for jQuery
  * Example cropping script
- * @copyright 2008-2009 Kelly Hallman
+ * @copyright 2008 Kelly Hallman
  * More info: http://deepliquid.com/content/Jcrop_Implementation_Theory.html
  */
 
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 	$targ_w = $targ_h = 150;
 	$jpeg_quality = 90;
 
-	$src = 'demo_files/pool.jpg';
+	$src = 'demo_files/flowers.jpg';
 	$img_r = imagecreatefromjpeg($src);
 	$dst_r = ImageCreateTrueColor( $targ_w, $targ_h );
 
@@ -27,75 +27,54 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
 // If not a POST request, display page below:
 
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Live Cropping Demo</title>
-  <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
-  <script src="../js/jquery.min.js"></script>
-  <script src="../js/jquery.Jcrop.js"></script>
-  <link rel="stylesheet" href="demo_files/main.css" type="text/css" />
-  <link rel="stylesheet" href="demo_files/demos.css" type="text/css" />
-  <link rel="stylesheet" href="../css/jquery.Jcrop.css" type="text/css" />
+?><html>
+	<head>
 
-<script type="text/javascript">
+		<script src="../js/jquery.pack.js"></script>
+		<script src="../js/jquery.Jcrop.pack.js"></script>
+		<link rel="stylesheet" href="../css/jquery.Jcrop.css" type="text/css" />
+		<link rel="stylesheet" href="demo_files/demos.css" type="text/css" />
 
-  $(function(){
+		<script language="Javascript">
 
-    $('#cropbox').Jcrop({
-      aspectRatio: 1,
-      onSelect: updateCoords
-    });
+			$(function(){
 
-  });
+				$('#cropbox').Jcrop({
+					aspectRatio: 1,
+					onSelect: updateCoords
+				});
 
-  function updateCoords(c)
-  {
-    $('#x').val(c.x);
-    $('#y').val(c.y);
-    $('#w').val(c.w);
-    $('#h').val(c.h);
-  };
+			});
 
-  function checkCoords()
-  {
-    if (parseInt($('#w').val())) return true;
-    alert('Please select a crop region then press submit.');
-    return false;
-  };
+			function updateCoords(c)
+			{
+				$('#x').val(c.x);
+				$('#y').val(c.y);
+				$('#w').val(c.w);
+				$('#h').val(c.h);
+			};
 
-</script>
-<style type="text/css">
-  #target {
-    background-color: #ccc;
-    width: 500px;
-    height: 330px;
-    font-size: 24px;
-    display: block;
-  }
+			function checkCoords()
+			{
+				if (parseInt($('#w').val())) return true;
+				alert('Please select a crop region then press submit.');
+				return false;
+			};
 
+		</script>
 
-</style>
+	</head>
 
-</head>
-<body>
+	<body>
 
-<div class="container">
-<div class="row">
-<div class="span12">
-<div class="jc-demo-box">
+	<div id="outer">
+	<div class="jcExample">
+	<div class="article">
 
-<div class="page-header">
-<ul class="breadcrumb first">
-  <li><a href="../index.html">Jcrop</a> <span class="divider">/</span></li>
-  <li><a href="../index.html">Demos</a> <span class="divider">/</span></li>
-  <li class="active">Live Demo (Requires PHP)</li>
-</ul>
-<h1>Server-based Cropping Behavior</h1>
-</div>
+		<h1>Jcrop - Crop Behavior</h1>
 
 		<!-- This is the image we're attaching Jcrop to -->
-		<img src="demo_files/pool.jpg" id="cropbox" />
+		<img src="demo_files/flowers.jpg" id="cropbox" />
 
 		<!-- This is the form that our event handler fills -->
 		<form action="crop.php" method="post" onsubmit="return checkCoords();">
@@ -103,7 +82,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 			<input type="hidden" id="y" name="y" />
 			<input type="hidden" id="w" name="w" />
 			<input type="hidden" id="h" name="h" />
-			<input type="submit" value="Crop Image" class="btn btn-large btn-inverse" />
+			<input type="submit" value="Crop Image" />
 		</form>
 
 		<p>
@@ -113,8 +92,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 			dumped to the browser. Try it!
 		</p>
 
+		<div id="dl_links">
+			<a href="http://deepliquid.com/content/Jcrop.html">Jcrop Home</a> |
+			<a href="http://deepliquid.com/content/Jcrop_Manual.html">Manual (Docs)</a>
+		</div>
 
-	</div>
+
 	</div>
 	</div>
 	</div>
