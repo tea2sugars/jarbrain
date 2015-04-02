94265b4a4313fb117b645f7e5498b907f9696cdb
Rik Barwick <rik@creativitea.co.uk>
2015-04-02 11:40:25 +0100

    Editing Breadcrumb Template to work from URL

--- public/sites/all/themes/jarbrain/template.php
+++ public/sites/all/themes/jarbrain/template.php
@@ -10,3 +10,62 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+ 
+ <?php
+/**
+ * Override theme_breadcrumb().
+ */
+function jarbrain_breadcrumb($breadcrumb) {
+  $links = array();
+  $path = '';
+  
+
+// Get URL arguments
+  $arguments = explode('/', request_uri());
+  
+
+// Remove empty values
+  foreach ($arguments as $key => $value) {
+    if (empty($value)) {
+      unset($arguments[$key]);
+    }
+  }
+  $arguments = array_values($arguments);
+  
+
+// Add 'Home' link
+  $links[] = l(t('Home'), '<front>');
+  
+
+// Add other links
+  if (!empty($arguments)) {
+    foreach ($arguments as $key => $value) {
+      // Don't make last breadcrumb a link
+      if ($key == (count($arguments) - 1)) {
+        $links[] = drupal_get_title();
+      } else {
+        if (!empty($path)) {
+          $path .= '/'. $value;
+        } else {
+          $path .= $value;
+        }
+        $links[] = l(drupal_ucfirst($value), $path);
+      }
+    }
+  }
+  
+
+// Set custom breadcrumbs
+  drupal_set_breadcrumb($links);
+  
+
+// Get custom breadcrumbs
+  $breadcrumb = drupal_get_breadcrumb();
+  
+
+// Hide breadcrumbs if only 'Home' exists
+  if (count($breadcrumb) > 1) {
+    return '<div class="breadcrumb">'. implode(' &raquo; ', $breadcrumb) .'</div>';
+  }
+}
+?>
